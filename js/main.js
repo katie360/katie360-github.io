4f19334ad2bc066edb91f70e8d8be8820174fb11
katie360 <kateratemo44@gmail.com>
2025-04-04 18:44:23 +0300

    added local marked.min.js for markdown rendering

--- js/main.js
+++ js/main.js
@@ -9,7 +9,7 @@ const blogPosts = [
         readTime: "5 min read",
         tags: ["API Development", "Software Testing", "Intermediate "],
         image: "posts/post1/what-is-end-to-end-testing.webp",
-        markdownFile: "posts/post1/2025-04-01-End-to-End API Testing.md"
+        markdownFile: "/posts/post1/2025-04-01-End-to-End-API.md"
     },
     {
         id: 'post2',
@@ -20,7 +20,7 @@ const blogPosts = [
         readTime: "8 min read",
         tags: ["Backend Optimization", "Frontend Performance", "API Best Practices "],
         image: "posts/post2/API-Integration.png",
-        markdownFile: "posts/post2/2025-04-04 -Why Your API Might Be Slowing Down the Frontend (and How to Fix It).md"
+        markdownFile: "/posts/post2/2025-04-04-api-performance.md"
     }
 ];
 // Portfolio items
